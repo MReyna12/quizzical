eac9980956ec298534d75b2c231cc530c7a8cee2
Michael <MReyna12@live.com>
2022-08-09 12:41:59 -0500

    add animation to the quiz image for styling purposes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import quizImg from "/quiz.png";
 import "./Home.css";
 
 function Home() {
@@ -7,6 +8,11 @@ function Home() {
       <div className="blobs yellow-blob"></div>
       <div className="blobs blue-blob"></div>
       <div className="centered">
+        <img
+          className="spin"
+          src={quizImg}
+          alt="An icon that displays the text quiz with generic boxes and lines representing text"
+        />
         <h1>Quizzical</h1>
         <p className="top-bottom-spacing">Test your trivia skills!</p>
         <button>Start quiz</button>
