ecb03f9eb10ba68dcdb89a1630c974c99e8fffb3
Michael <MReyna12@live.com>
2022-08-09 11:47:52 -0500

    landing page created and blobs added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,10 +3,14 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="centered">
-      <h1>Quizzical</h1>
-      <p>Test your trivia skills!</p>
-      <button>Start quiz</button>
+    <div>
+      <div className="blobs yellow-blob"></div>
+      <div className="blobs blue-blob"></div>
+      <div className="centered">
+        <h1>Quizzical</h1>
+        <p className="top-bottom-spacing">Test your trivia skills!</p>
+        <button>Start quiz</button>
+      </div>
     </div>
   );
 }
